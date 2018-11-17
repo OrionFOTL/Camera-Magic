7addd59454a8362bdf1899a63494c5ebdf1675c2
Orion <8158804+OrionFOTL@users.noreply.github.com>
2018-11-17 18:53:20 +0100

    Hello world

---
+++ app/src/main/java/com/example/krzysiek/myfirstapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.krzysiek.myfirstapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        boolean kamera = checkCameraHardware(this);
+        
+        setContentView(R.layout.activity_main);
+    }
+    /** Sprawdzenie czy urzadzenie ma kamere **/
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+}

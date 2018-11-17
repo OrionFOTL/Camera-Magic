c21e38abd2cf3d78a29a91d3fe5c5d8ece569212
Orion <8158804+OrionFOTL@users.noreply.github.com>
2018-11-17 23:27:34 +0100

    Sprawdzanie uprawnień do kamery

--- app/src/main/java/com/example/krzysiek/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/krzysiek/myfirstapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.krzysiek.myfirstapp;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,34 +18,81 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public static final int PERMISSIONS_REQUEST_CAMERA = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        checkCameraHardware(this); //sprawdz czy jest kamera
         setContentView(R.layout.activity_main);
+
+        //jeśli nie ma pozwolenia na kamerę
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},PERMISSIONS_REQUEST_CAMERA); //poproś o pozwolenie
+        }
+        else{ //jeśli zgoda już była udzielona
+            checkCameraHardware(this); //sprawdz czy jest kamera
+            getCameraInstance();
+        }
     }
-    /** Sprawdzenie czy urzadzenie ma kamere **/
+
+    public Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open();
+        }
+        catch (Exception e){
+            displayErrorModal("Błąd kamery",e.toString());
+        }
+        return c;
+    }
+
     private boolean checkCameraHardware(Context context) {
         if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
             // this device has a camera
             return true;
         } else {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Brak kamery");
-            builder.setMessage("Twój telefon nie ma aparatu");
-            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    finish();
-                    System.exit(0);
-                }
-            });
-            builder.setCancelable(false);
-            AlertDialog dialog = builder.create();
-            dialog.setCanceledOnTouchOutside(false);
-            dialog.show();
+            displayErrorModal("Brak kamery","Twój telefon nie ma aparatu");
             return false;
         }
+    } //true jeśli urządzenie ma kamerę
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults){
+        switch(requestCode){
+            case PERMISSIONS_REQUEST_CAMERA: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    checkCameraHardware(this); //sprawdz czy jest kamera
+                    getCameraInstance();
+                    return;
+                }
+                else displayErrorModal("Błąd uprawnień", "Nie pozwoliłeś na dostęp do kamery");
+            }
+            //inne case dla requestCode'ów innych przyznanych uprawnień
+        }
+    }
+    public void displayErrorModal(String title, String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+                System.exit(0);
+            }
+        });
+        builder.setCancelable(false);
+        AlertDialog dialog = builder.create();
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+    }
+    public void displayModal(String title, String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.setPositiveButton("OK", null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     /** Called when the user taps the Send button */
@@ -51,6 +102,5 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
-
     }
 }

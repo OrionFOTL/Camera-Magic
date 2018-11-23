54194748e7de7a2675107ae559fabe8a26b63747
Orion <8158804+OrionFOTL@users.noreply.github.com>
2018-11-23 23:42:46 +0100

    Chyba działa pytanie o uprawnienia

--- app/src/main/java/com/example/krzysiek/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/krzysiek/myfirstapp/MainActivity.java
@@ -6,20 +6,56 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     public static final int PERMISSIONS_REQUEST_CAMERA = 1;
+    public static final int PERMISSIONS_REQ_WRITESTORAGE = 5;
     private Camera mCamera;
     private CameraPreview mPreview;
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null){
+                displayModal("Błąd przy tworzeniu pliku","Błąd zapisu zdjęcia, sprawdź uprawnienia dostępu do pamięci");
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                displayModal("Nie znaleziono pliku",e.getMessage());
+            } catch (IOException e) {
+                displayModal("Błąd dsotępu do pliku",e.getMessage());
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +69,28 @@ public class MainActivity extends AppCompatActivity {
         else{ //jeśli zgoda już była udzielona
             initCamera();
         }
+        Button captureButton = (Button) findViewById(R.id.captureButton);
+        captureButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // get an image from the camera
+                        if (ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                                != PackageManager.PERMISSION_GRANTED){
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},PERMISSIONS_REQ_WRITESTORAGE);
+                        }
+                        else mCamera.takePicture(null, null, mPicture);
+                    }
+                }
+        );
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();/*
+        if (mCamera!=null) {
+            mCamera.release();
+            mCamera = null;
+        }*/
     }
 
     public void initCamera(){
@@ -47,7 +105,6 @@ public class MainActivity extends AppCompatActivity {
         preview.addView(mPreview);
 
     }
-
     public Camera getCameraInstance() {
         Camera cam = null;
         try {
@@ -71,16 +128,20 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults){
-        switch(requestCode){
-            case PERMISSIONS_REQUEST_CAMERA: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    initCamera();
-                    return;
-                }
-                else displayErrorModal("Błąd uprawnień", "Nie pozwoliłeś na dostęp do kamery");
-            }
-            //inne case dla requestCode'ów innych przyznanych uprawnień
+        if (requestCode == PERMISSIONS_REQUEST_CAMERA) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initCamera();
+                return;
+            } else displayErrorModal("Błąd uprawnień", "Nie pozwoliłeś na dostęp do kamery");
         }
+        if (requestCode == PERMISSIONS_REQ_WRITESTORAGE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mCamera.takePicture(null, null, mPicture);
+                return;
+            }
+            else displayErrorModal("Błąd uprawnień", "Nie pozwoliłeś na dostęp do pamięci");
+        }
+        //inne case dla requestCode'ów innych przyznanych uprawnień
     }
     public void displayErrorModal(String title, String message){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -106,6 +167,38 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog dialog = builder.create();
         dialog.show();
     }
+
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
 
     /* Called when the user taps the Send button */
     /*
